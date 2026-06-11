4ee6a6c13ae79439174a49649cd8f45ad6502047
deepucoder25 <deepalikumari25@navgurukul.org>
2026-06-11 13:29:17 +0530

    change

--- application/modules/template/views/footer.php
+++ application/modules/template/views/footer.php
@@ -261,6 +261,6 @@
 <?php $this->load->view('contacts/callmeback_modal'); ?>
 <?php $this->load->view('contacts/review_modal'); ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?=base_url()?>assets/js/form.js"></script>
+<script src="<?=base_url()?>assets/js/form.js?v=1.2"></script>
 </body>
 </html>
--- application/modules/template/views/header.php
+++ application/modules/template/views/header.php
@@ -144,5 +144,5 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
   <!-- CSS and Java Script -->
-  <link rel="stylesheet" href="<?= base_url("assets/css/style.css") ?>">
+    <link rel="stylesheet" href="<?= base_url("assets/css/style.css?v=1.2") ?>">
 </head>
